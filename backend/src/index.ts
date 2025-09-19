6a56edb45dc8c0fa735a2ca2c09aa60814fff1c6
taras-rm <tarasrom25@gmail.com>
2025-09-18 23:47:57 -0400

    prisma setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,16 @@
 import express from "express"
 
 import type { Request, Response } from "express"
+import client from "./db/index.ts"
 
-const PORT = 3001
+const PORT = 3000
 
 const app = express()
 
 app.use(express.json())
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello world!!dx!")
+    res.send(async () => (await client.user.findMany()))
 })
 
 app.listen(PORT, () => {
